6384da51d230688a05e0a04fc91fdea937972e44
Shaik-Shadaf <saiksadaf2023@gmail.com>
2025-03-01 11:48:12 +0530

    Revert "google-sheets-clone-web-application"
    This reverts commit 32228ed7fe69ab3522845cb0ece5e82af0cdda86.

--- project-root/backend/models/Spreadsheet.js
+++
@@ -1,8 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const SpreadsheetSchema = new mongoose.Schema({
-  data: { type: Array, required: true },
-});
-
-module.exports = mongoose.model('Spreadsheet', SpreadsheetSchema);
